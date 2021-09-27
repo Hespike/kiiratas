766884739f2aaa44cd474546935993e3d50113f8
Sárdi András <sardi.andras@gmail.com>
2021-09-27 11:38:29 +0200

    Create kiiratas.cpp

---
+++ kiiratas.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hetfo\nKedd\nSzerda\nCsutortok\nPentek\n";
+    cout << "Hetfo, Kedd, Szerda, Csutortok, Pentek\n";
+    
+    return 0;
+}
